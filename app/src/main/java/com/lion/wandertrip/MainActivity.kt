8916c86a491fa39cfa571c780a7171364a957ff7
skgmlwls <127082621+skgmlwls@users.noreply.github.com>
2025-02-19 09:07:19 +0900

    관광지 항목 진행중

--- app/src/main/java/com/lion/wandertrip/MainActivity.kt
+++ app/src/main/java/com/lion/wandertrip/MainActivity.kt
@@ -77,8 +77,7 @@ fun MyApp() {
 
     NavHost(
         navController = rememberNavHostController,
-        startDestination = "${ScheduleScreenName.SCHEDULE_DETAIL_SCREEN.name}?" +
-                "tripScheduleDocId=jgbGuyxXMAj8nvChmAyN&areaName=서울&areaCode=1"
+        startDestination = MainScreenName.MAIN_SCREEN_START.name
     ) {
         composable(MainScreenName.MAIN_SCREEN_START.name) { StartScreen() }
         // 일정 메인 화면
