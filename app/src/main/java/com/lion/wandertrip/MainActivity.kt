91caeffc28de1bf633e07c7d93b7117418ace3b1
skgmlwls <127082621+skgmlwls@users.noreply.github.com>
2025-02-21 09:07:20 +0900

    관광지 룰렛 완료

--- app/src/main/java/com/lion/wandertrip/MainActivity.kt
+++ app/src/main/java/com/lion/wandertrip/MainActivity.kt
@@ -82,8 +82,7 @@ fun MyApp() {
 
     NavHost(
         navController = rememberNavHostController,
-        startDestination = "${ScheduleScreenName.SCHEDULE_DETAIL_SCREEN.name}?" +
-                "tripScheduleDocId=jgbGuyxXMAj8nvChmAyN&areaName=서울&areaCode=1"
+        startDestination = MainScreenName.MAIN_SCREEN_START.name
     ) {
         composable(MainScreenName.MAIN_SCREEN_START.name) { StartScreen() }
         // 일정 메인 화면
