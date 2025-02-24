3fb9107748605f067bcaf66a76926ffc9c0a83bd
skgmlwls <127082621+skgmlwls@users.noreply.github.com>
2025-02-24 09:03:18 +0900

    일정 위치 조정 진행 중

--- app/src/main/java/com/lion/wandertrip/component/ShimmerPlaceholder.kt
+++ app/src/main/java/com/lion/wandertrip/component/ShimmerPlaceholder.kt
@@ -1,10 +1,12 @@
 package com.lion.wandertrip.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.valentinilk.shimmer.shimmer
 
 @Composable
@@ -14,6 +16,9 @@ fun ShimmerPlaceholder(
     Box(
         modifier = modifier
             .fillMaxSize()
-            .shimmer() // valentinilk.shimmer 의 확장 함수
+            // 🔹 배경 색상 추가 (연한 회색)
+            .background(Color(0xFFE0E0E0))
+            // 🔹 Shimmer 애니메이션
+            .shimmer(),
     )
 }
