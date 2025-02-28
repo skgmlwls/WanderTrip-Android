5f3bbec8b97335e14d84c6d528f3aea249091201
zero259579 <zero259579@gmail.com>
2025-02-28 14:20:02 +0900

    별점 리뷰 카테고리

--- app/src/main/java/com/lion/wandertrip/presentation/bottom/home_page/HomeScreen.kt
+++ app/src/main/java/com/lion/wandertrip/presentation/bottom/home_page/HomeScreen.kt
@@ -40,6 +40,7 @@ fun HomeScreen(
     val imageUrlMap = viewModel.imageUrlMap
     val isLoading by viewModel.isLoading.observeAsState(false) // ✅ 로딩 상태 감지
     val userModel by viewModel.userModel.observeAsState(UserModel(userDocId = "", userLikeList = emptyList()))
+    val contentsModelMap by viewModel.contentsModelMap.observeAsState(emptyMap())
 
     LaunchedEffect(Unit) {
         viewModel.fetchTripNotes()
@@ -105,6 +106,7 @@ fun HomeScreen(
                             tripItem = tripItem,
                             onItemClick = { viewModel.onClickTrip(tripItem.contentId) },
                             userModel = userModel,
+                            contentsModel = contentsModelMap[tripItem.contentId],
                             onFavoriteClick = { contentId -> viewModel.toggleFavorite(contentId) }
                         )
                     }
